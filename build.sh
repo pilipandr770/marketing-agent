555412d94adecc131c8be17ba970abd62ca7cf81
AndriiIT <andrii.it.info@gmail.com>
2025-10-08 14:23:20 +0200

    Add Render.com deployment configuration: build.sh, Procfile, requirements updated with PostgreSQL support

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run database migrations
+flask db upgrade
